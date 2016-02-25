40be905a7015936cf1036a73f0c69bd5c6ecee27
Sam Khalandovsky <samkhal@gmail.com>
2016-02-25 02:11:16 -0500

    floated visualization

--- visualize.m
+++ visualize.m
@@ -1,5 +1,8 @@
 % Visualize biped in Drake
 
-m = RigidBodyManipulator('urdf/Legs.urdf');
+options.floating = true;
+m = RigidBodyManipulator('urdf/Legs.urdf',options);
 v = m.constructVisualizer();
-v.inspector(zeros(24,1)) % Init inspector with all joints at 0
+v.inspector(zeros(24+12,1)) % Init inspector with all joints at 0
+
+% Note: body rolls and pitches wrong, likely frame is in the wrong place
