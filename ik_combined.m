b9df53a16a5521bd20612d65de7b01b06c97dc76
Ari-Goodman <abgoodman@wpi.edu>
2016-03-17 16:40:11 -0400

    updated to ensure toes stay flat

--- ik_combined.m
+++ ik_combined.m
@@ -11,10 +11,14 @@ nq = legs.getNumPositions(); %number of DOF
 pelvis = legs.findLinkId('pelvis');
 l_foot = legs.findLinkId('l_foot');
 r_foot = legs.findLinkId('r_foot');
+l_toe = legs.findLinkId('l_foot');
+r_toe = legs.findLinkId('r_foot');
 
 kc1r = WorldEulerConstraint(legs,pelvis,-Tol,Tol); %about upright
 kc2r = WorldEulerConstraint(legs,l_foot,[pi/2;0;nan]-Tol,[pi/2;0;nan]+Tol); %flat feet
 kc3r = WorldEulerConstraint(legs,r_foot,[pi/2;0;nan]-Tol,[pi/2;0;nan]+Tol);
+kc4r = WorldEulerConstraint(legs,l_toe,[pi/2;0;nan]-Tol,[pi/2;0;nan]+Tol);
+kc5r = WorldEulerConstraint(legs,r_toe,[pi/2;0;nan]-Tol,[pi/2;0;nan]+Tol);
 %0 .1 0.09, 0 .1 .1
 kc1p = WorldPositionConstraint(legs,l_foot,[0;0;0],LPos-Tol,LPos+Tol);
 kc2p = WorldPositionConstraint(legs,r_foot,[0;0;0],RPos-Tol,RPos+Tol);
@@ -27,7 +31,7 @@ ikoptions = IKoptions(legs);
 ikoptions = ikoptions.setDebug(true);
 ikoptions = ikoptions.setMex(false);
 ikoptions = ikoptions.setMajorIterationsLimit(5000);
-ikproblem = InverseKinematics(legs,q_nom,kc1c,kc1r,kc2r,kc3r,kc1p,kc2p);
+ikproblem = InverseKinematics(legs,q_nom,kc1c,kc1r,kc2r,kc3r,kc4r,kc5r,kc1p,kc2p);
 ikproblem = ikproblem.setQ(ikoptions.Q);
 
 ikproblem = ikproblem.setSolverOptions('fmincon','Algorithm','sqp');
@@ -35,7 +39,6 @@ ikproblem = ikproblem.setSolverOptions('fmincon','MaxIter',500);
 
 tic;
 [qik,F,info,infeasible_cnstr_ik] = ikproblem.solve(q_seed);
-toc
 
 v = legs.constructVisualizer();
 v.inspector([qik;zeros(18,1)])
