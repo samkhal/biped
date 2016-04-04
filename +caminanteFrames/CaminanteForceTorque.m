0cf4b0f39e2419fbbbb11aa326677372872ad7d4
Sam Khalandovsky <samkhal@gmail.com>
2016-04-04 19:11:22 -0400

    minor comment

--- +caminanteFrames/CaminanteForceTorque.m
+++ +caminanteFrames/CaminanteForceTorque.m
@@ -3,6 +3,7 @@ classdef CaminanteForceTorque < SingletonCoordinateFrame
   methods
     function obj=CaminanteForceTorque()
 
+        %!! should be set up, currently not called by anything
 %       coordinates = {'l_foot_fz','l_foot_tx','l_foot_ty',...
 %                 'r_foot_fz','r_foot_tx','r_foot_ty'};
 
