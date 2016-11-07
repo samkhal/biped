be1f26ede4bf49b44c390b8b302526ce726a5cdf
Sam Khalandovsky <samkhal@gmail.com>
2016-11-06 19:01:31 -0500

    export utility

--- util/exportTrajectory.m
+++ util/exportTrajectory.m
@@ -4,10 +4,14 @@
 states = ytraj.xx;
 times = ytraj.tt;
 assert(size(states,2)==size(times,2));
-assert(size(states,2)<find(isnan(torques(1,:)),1)); % nans in torque should start after the length of states
+if any(isnan(torques))
+    assert(size(states,2)<find(isnan(torques(1,:)),1)); % nans in torque should start after the length of states
+end
 torques = torques(:,1:size(states,2));
 
 state_frames = r.getOutputFrame.getCoordinateNames;
 torque_frames = r.getInputFrame.getCoordinateNames;
 
-save(filename, 'states', 'times', 'torques', 'state_frames', 'torque_frames');
+% global notes variable should have a string with relevant info
+
+save(filename, 'states', 'times', 'torques', 'state_frames', 'torque_frames', 'notes');
