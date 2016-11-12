cdc005748fcc78c509ff14a99b57460f76b79ade
Ari-Goodman <abgoodman@wpi.edu>
2016-11-12 11:40:40 -0500

    fixed typo in test.m swapped constant ranges.  edited find optimal energy system to include output description

--- energy_calc/test.m
+++ energy_calc/test.m
@@ -1,4 +1,4 @@
-constant_range = [-6000 -.7; 6000 .2];
+constant_range = [-.6577 -6000 ;.2  6000 ];
 joints = {'kny'};
 foo = @(angle,constants)((angle+constants(1))*constants(2));
-[const savings] = find_optimal_energy_system(states, state_frames, torques, torque_frames, foo, constant_range, joints);
+[const, savings] = find_optimal_energy_system(states, state_frames, torques, torque_frames, foo, constant_range, joints);
