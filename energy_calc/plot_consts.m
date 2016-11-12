9a6a599bbeb4926c6901e74189c0e81b8356676e
Ari-Goodman <abgoodman@wpi.edu>
2016-11-12 14:40:55 -0500

    fixed error in plot consts

--- energy_calc/plot_consts.m
+++ energy_calc/plot_consts.m
@@ -2,9 +2,9 @@
 figure(11)
 cla;
 hold on;
-for i = 2:length(consts)
+for i = 1:length(consts)
 %for i = 13
     x = linspace(.67,pi/2,100);
-    plot(x,polyval(consts{i-1},x+consts{1}));
+    plot(x,polyval(consts{i}(2:end),x+consts{i}(1)));
     pause(1)
 end
