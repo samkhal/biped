603a87fbdcf477d3504370f703b450fa9cf934ab
Ari-Goodman <abgoodman@wpi.edu>
2016-11-12 19:13:59 -0500

    modified test to propperlly show akx error. run test. look at max power

--- energy_calc/test.m
+++ energy_calc/test.m
@@ -1,8 +1,8 @@
-%consts = {};
+consts = {};
 %for i = 2:35
     constant_range = repmat([-6000;6000],1,2);
     joints = {'akx'};
-    foo = @(angle,constants)(abs(angle+constants(1)).^2).*constants(2);%(polyval(constants(2:end),angle+constants(1)));
+    foo = @(angle,constants)((angle-.3825)).*constants(1);%(polyval(constants(2:end),angle+constants(1)));
     [const, savings] = find_optimal_energy_system(states, state_frames, torques, torque_frames, foo, constant_range, joints);
     consts{end+1} = const;
     min(min(foo(states([14,9],:),const)))
