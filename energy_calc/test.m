860cbb3bfd05e6595c780b54db976a03611f3cbf
Ari-Goodman <abgoodman@wpi.edu>
2016-11-12 19:08:52 -0500

    edited test to show akx has too high of max pow. did not check if angles were within propper range.

--- energy_calc/test.m
+++ energy_calc/test.m
@@ -1,11 +1,11 @@
-consts = {};
-for i = 2:35
-    constant_range = repmat([-6000;6000],1,i);
-    joints = {'kny'};
-    foo = @(angle,constants)(polyval(constants(2:end),angle+constants(1)));
+%consts = {};
+%for i = 2:35
+    constant_range = repmat([-6000;6000],1,2);
+    joints = {'akx'};
+    foo = @(angle,constants)(abs(angle+constants(1)).^2).*constants(2);%(polyval(constants(2:end),angle+constants(1)));
     [const, savings] = find_optimal_energy_system(states, state_frames, torques, torque_frames, foo, constant_range, joints);
     consts{end+1} = const;
     min(min(foo(states([14,9],:),const)))
     hold on;
     plot(i,savings(1),'*');
-end
+%end
