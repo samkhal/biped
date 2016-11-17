7a93a7a34fb2f6cc04fd97f434f32d9701178fc9
Ari-Goodman <abgoodman@wpi.edu>
2016-11-16 22:08:19 -0500

    added plot. still takes a few seconds to run but this is not a priority.

--- energy_calc/bestTorquevAngle.m
+++ energy_calc/bestTorquevAngle.m
@@ -1,18 +1,17 @@
 %% Ari Goodman
 %% 11/16/2016
 
-%% THIS FILE IS STILL UNDER CONSTRUCTION
-
 %% for a given trajectory, finds the optimal torques at each angle to minimizes energy consumption for a single joint.
 
 %% inputs
-% map  = (bin, {[position-rad speed-rad/s torque-Nm]...})
+% joint: name of joint being optimized
+% traj, trajectory input
 
 %% outputs
-% best_torques matrix of best torques-Nm
+% best_torques matrix of best torques-Nm separated into a positive and
+% negative function
 
 function best_torques = bestTorquevAngle(joint, traj)
-    tic
     sortedStates = sortrows([[traj.pos.(joint)]', [traj.vel.(joint)]', [traj.torque.(joint)]']);
     edges = min([traj.pos.(joint)]):1/100*(max([traj.pos.(joint)])-min([traj.pos.(joint)])):max([traj.pos.(joint)]);
     bins = discretize(sortedStates(:,1),edges);
@@ -36,9 +35,16 @@ function best_torques = bestTorquevAngle(joint, traj)
     best_torques = zeros(size(map,1),1);
     for i = 1:size(map,1)
         tempBin = map(i);
-        energyFun = @(input_torque)(sum(electrical_power([M,R],tempBin(2,:),tempBin(3,:)-input_torque,0)));
-        best_torques(i) = fminbnd(energyFun, min(tempBin(3,:)),max(tempBin(3,:)));       
+        energyFun = @(input_torque)(sum(electrical_power([M,R],tempBin(2,:),abs(tempBin(3,:)-input_torque),0)));
+        best_torques(i,1) = fminbnd(energyFun, min(0, min(tempBin(3,:))),min(0,max(tempBin(3,:))));      
+        best_torques(i,2) = fminbnd(energyFun, max(0, min(tempBin(3,:))),max(0,max(tempBin(3,:))));       
     end
-    %TODO ADD A PLOT
-    
+    figure;
+    hold on;
+    plot(edges(1:(end-1)),best_torques(:,1),'r')
+    plot(edges(1:(end-1)),best_torques(:,2),'b')
+    xlabel('angle rad');
+    ylabel('torque Nm');
+    title('best input torque v angle');
+    legend( 'negative','positive');
 end
