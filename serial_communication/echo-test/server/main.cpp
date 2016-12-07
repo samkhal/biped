f50a92a610c0cdc6250bf651282b83be2f743982
Sam Khalandovsky <samkhal@gmail.com>
2016-12-07 14:20:32 -0500

    Removed old serial stuff

--- serial_communication/echo-test/server/main.cpp
+++ serial_communication/echo-test/server/main.cpp
@@ -2,7 +2,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#include "arduino-serial-lib.h"
 #include <SerialStream.h>
 #include <iostream>
 
@@ -24,41 +23,5 @@ int main(){
 		serial >> recv;
 		std::cout<<"Received "<<(int)recv<<std::endl;
 	}
-	char msg2 = 1;
-	std::cout<< "Sending"<<(int)msg2 <<std::endl;
-
-	serial << msg2;
-
-	unsigned char recv;
-	serial >> recv;
-	std::cout<<(int)recv<<std::endl;
 	return 0;
-
-
-}
-
-int simple_serial_echo(){
-	printf("Start\n");
-	int serial = serialport_init("/dev/ttyACM0", 115200);
-
-	// Write a character to the Teensy
-	char send[] = "t";
-	write(serial, send, 1);
-	printf("Sent: %c\n", send[0]);
-
-	// Wait for the echo
-	char out[1];
-	int n;
-	do{
-		n = read(serial, out, 1);
-		if(n==-1){
-			printf("failed to read\n");
-			return -1;
-		}
-		usleep(1000); //wait 1 ms
-	} while (n<1);
-
-	printf("Received: %c\n",out[0]);
-
-	close(serial);
 }
