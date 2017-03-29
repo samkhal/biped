39b972e756435b6887eb0b2406ac4c29deb77e9d
Ari <abgoodman@wpi.edu>
2017-03-28 20:35:23 -0400

    added simple grav comp. to be integrated with gui

---
+++ plan_control/simpleGravComp.m
@@ -0,0 +1,23 @@
+%Ari Goodman
+%3/28/2017
+
+%Given the current position and velocities, solves for the compensation
+%torque for each joint.
+
+function torque = simpleGravComp(q,v)
+   options.floating = true;
+   options.dt = 0.001;
+   options.terrain = RigidBodyFlatTerrain;
+    r = Caminante('urdf/caminante_minimal.urdf',options);
+    r = compile(r);
+    if nargin ==0
+        [H C B dH dC dB] = r.manipulatorDynamics([0, 0, 0.6010, 0.0001, -0.0028, 0, -0.0143, 0.0009, 0.0009, 0.0009, -0.0091, -0.0144, 0.0009, 0.0009, 0.0009, 0.0089]', zeros(r.getNumVelocities,1));
+    elseif nargin == 1
+        [H C B dH dC dB] = r.manipulatorDynamics(q, zeros(r.getNumVelocities,1));
+    else
+        [H C B dH dC dB] = r.manipulatorDynamics(q, v);
+    end
+    
+%H(q)*vd+C(q,v,f_ext)=B(q,v)*tau
+torque = C;
+end
